netconstructor/OpenLayers-1

commit 539b73ed648079f512c0b67a9aa4891c11140303

applyDefaults now respects null - if you want to override a property with applyDefaults, set it to undefined first - applyDefaults also now correctly applies a custom toString method (closes #1063).
git-svn-id: http://svn.openlayers.org/trunk/openlayers@5280 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers/Util.js
@@ -489,20 +489,37 @@ OpenLayers.Util.upperCaseObject = function (object) {
 
 /** 
  * Function: applyDefaults
- * Takes a hashtable and copies any keys that don't exist from
- *     another hashtable, by analogy with OpenLayers.Util.extend() from
+ * Takes an object and copies any properties that don't exist from
+ *     another properties, by analogy with OpenLayers.Util.extend() from
  *     Prototype.js.
  * 
  * Parameters:
- * to - {Object}
- * from - {Object}
+ * to - {Object} The destination object.
+ * from - {Object} The source object.  Any properties of this object that
+ *     are undefined in the to object will be set on the to object.
+ *
+ * Returns:
+ * {Object} A reference to the to object.  Note that the to argument is modified
+ *     in place and returned by this function.
  */
 OpenLayers.Util.applyDefaults = function (to, from) {
     for (var key in from) {
-        if (to[key] == null) {
+        if (to[key] === undefined ||
+            (from.hasOwnProperty
+             && from.hasOwnProperty(key) && !to.hasOwnProperty(key))) {
             to[key] = from[key];
         }
     }
+    /**
+     * IE doesn't include the toString property when iterating over an object's
+     * properties with the for(property in object) syntax.  Explicitly check if
+     * the source has its own toString property.
+     */
+    if(from.hasOwnProperty
+       && from.hasOwnProperty('toString') && !to.hasOwnProperty('toString')) {
+        to.toString = from.toString;
+    }
+    
     return to;
 };
 

tests/test_Util.html
@@ -141,18 +141,21 @@
 
     }
 
-    function test_06_Util_applyDefaults(t) {
+    function test_Util_applyDefaults(t) {
     
-        t.plan(8);
+        t.plan(10);
         
         var to = { 
             'a': "abra",
-            'b': "blorg"
+            'b': "blorg",
+            'n': null
         };
 
         var from = { 
             'b': "zoink",
-            'c': "press"
+            'c': "press",
+            'toString': function() {return 'works'},
+            'n': "broken"
         };
 
         OpenLayers.Util.applyDefaults(to, from);
@@ -167,6 +170,8 @@
         t.eq( ret["a"], "abra", "key present in ret but not from maintained");
         t.eq( ret["b"], "blorg", "key present in ret and from, maintained in ret");
         t.eq( ret["c"], "press", "key present in from and not ret successfully copied to ret");
+        t.eq(to.toString(), "works", "correctly applies custom toString");
+        t.eq(to.n, null, "correctly preserves null");
     }
 
     function test_07_Util_getParameterString(t) {

commit 0e34e00acc33714fb06f6b38262101d9c9c60e38

fixing regression committed in r5280. applyDefaults() again allows for an undefined value as the 'from' parameter. thanks tim for report and review. (Closes #1716)
git-svn-id: http://svn.openlayers.org/trunk/openlayers@7943 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

lib/OpenLayers/Util.js
@@ -563,7 +563,7 @@ OpenLayers.Util.applyDefaults = function (to, from) {
      * properties with the for(property in object) syntax.  Explicitly check if
      * the source has its own toString property.
      */
-    if(!fromIsEvt && from.hasOwnProperty
+    if(!fromIsEvt && from && from.hasOwnProperty
        && from.hasOwnProperty('toString') && !to.hasOwnProperty('toString')) {
         to.toString = from.toString;
     }

tests/Util.html
@@ -169,7 +169,7 @@
 
     function test_Util_applyDefaults(t) {
     
-        t.plan(11);
+        t.plan(12);
         
         var to = { 
             'a': "abra",
@@ -204,6 +204,15 @@
         
         var ret = OpenLayers.Util.applyDefaults(to, from);
         t.eq(ret.rand, from.rand, "works with undefined to");
+
+        //regression test for #1716 -- allow undefined from
+        try {
+            OpenLayers.Util.applyDefaults({}, undefined);
+            t.ok(true, "no exception thrown when from is undefined");
+        } catch(err) {
+            t.fail("exception thrown when from is undefined:" + err);
+        }
+
     }
 
     function test_Util_getParameterString(t) {